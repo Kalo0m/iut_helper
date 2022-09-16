df47124afa79137f0d7e8ab545df8524a2343000
Théo Letouzé <theo.letouze44@gmail.com>
2022-09-17 00:41:22 +0200

    coming soon ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 
 // create hello world react component
 export default function Index() {
-  return <h1 className=" text-amber-400">Hello World</h1>;
+  const [opacity, setOpacity] = useState(50);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOpacity(lastvalue => lastvalue === 100 ? 50 : 100);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
+
+  return <>
+    <h1 className={`text-green-500 transition-all duration-1000 text-center pt-10 text-3xl opacity-${opacity}`}>Coming soon ...</h1>
+  </>
 }
 
